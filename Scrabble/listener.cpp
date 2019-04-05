df91941b9093a5b9434c7ea05b9b6159049b0daa
Kevin <magneticok@gmail.com>
2019-04-04 22:48:40 -0600

    Missing files

---
+++ Scrabble/listener.cpp
@@ -0,0 +1,16 @@
+#include "listener.h"
+
+Listener::Listener()
+{
+
+}
+
+
+void Listener::listen(){
+    Client::readFromServer(Client::buffer);
+}
+
+
+/* Voy a tener varios signals segun las diferentes acciones que puedo realizar
+ * Voy a tener varios slots en Game segun las diferentes acciones que puedo realizar
+*/
